2c144e7df50ce4e8bd99e7308ef2a2bd3d4f3437
Charlieee1 <77288859+Charlieee1@users.noreply.github.com>
2024-04-26 09:32:43 -0700

    Create Boxel 3d Speed Indicator.user.js

---
+++ Boxel 3d Speed Indicator.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Boxel 3d Speed Indicator
+// @namespace    http://tampermonkey.net/
+// @version      v1.0
+// @description  Adding a speed indicator to boxel 3d
+// @author       Charlieee1
+// @match        https://www.dopplercreative.com/test/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+  
+    // Indicator
+    let indicator = document.createElement("div");
+    var text = document.createTextNode("0");
+    indicator.appendChild(text);
+    indicator.style.position = "absolute";
+    indicator.style.bottom = 0;
+    indicator.style.left = 0;
+    indicator.style.paddingLeft = 0;
+    indicator.style.paddingBottom = 0;
+    indicator.style.fontSize = "20px";
+    indicator.style.backgroundColor = "white";
+    appElement.appendChild(indicator);
+
+    addUpdateFunction(function() {
+      text.textContent = getPlayerSpeed().toFixed(1).toString();
+    });
+})();
