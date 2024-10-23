6f01c02af2ff9ca6a72e11fd2d7d67dc84f6d23e
Charlieee1 <77288859+Charlieee1@users.noreply.github.com>
2024-10-23 08:19:53 -0700

    Update Boxel 3d Time Manipulator.user.js

--- Boxel 3d Time Manipulator.user.js
+++ Boxel 3d Time Manipulator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Boxel 3d Time Manipulator
 // @namespace    http://tampermonkey.net/
-// @version      v1.1
+// @version      v1.2
 // @description  Time manipulator library for boxel 3d
 // @author       Charlieee1
 // @match        *dopplercreative.com/test/*
@@ -9,14 +9,18 @@
 // @grant        none
 // ==/UserScript==
 
+// Level starting (entering or restarting a level)
 var beforeLevelStart = [];
 var afterLevelStart = [];
-var beforeLevelRestart = [];
-var afterLevelRestart = [];
-var beforeFinish = [];
-var afterFinish = [];
+// Level restart (pressing "r" or dying/pressing "c" without a checkpoint)
+//var beforeLevelRestart = [];
+//var afterLevelRestart = [];
+// Respawning at checkpoint (pressing "c" or dying with a checkpoint)
 var beforeCheckpointRespawn = [];
 var afterCheckpointRespawn = [];
+// Finishing a level
+var beforeFinish = [];
+var afterFinish = [];
 
 function setTime(time) {
     app.timer.playTime = Date.now() - app.timer.startTime - 1e3 * time;
@@ -43,36 +47,50 @@ function resetTime() {
 (function() {
     'use strict';
 
-    app.playLevel = function() {
+    app.timer.reset = function() {
         beforeLevelStart.forEach((func) => func());
-        beforeLevelRestart.forEach((func) => func());
 
-        // START Doppler's code
-        app.play = true, app.timer.start()
-        // END Doppler's code
+		// START Doppler's code
+		var e = Date.now();
+		app.timer.startTime = e,
+		app.timer.pauseTime = e,
+		app.timer.playTime = 0
+		// END Doppler's code
 
         afterLevelStart.forEach((func) => func());
-        afterLevelRestart.forEach((func) => func());
     }
 
-    app.level.retryLevel = function(e = app, t = false) {
-        if (t == false) beforeLevelRestart.forEach((func) => func());
+	// HELP I can't figure out how to detect level restart
+	// This implementation detects level starts (above function) and checkpoint respawns (next function) - so it's useless
+    //app.level.resetLevel = function() {
+    //    beforeLevelRestart.forEach((func) => func());
+	//
+	//	// START Doppler's code
+	//	for (var e = 0; e < app.level.children.length; e++) {
+	//	    var t = app.level.children[e];
+	//	    t.resetToOrigin()
+	//	}
+	//	// END Doppler's code
+	//
+    //    afterLevelRestart.forEach((func) => func());
+    //}
 
-        // START Doppler's code
-        e.updateGravity(), e.play = true, e.level.removeParticles(e), e.player.cancelRestart(), e.resetScene(e), window.dispatchEvent(new CustomEvent("closePopup")), t == false || e.player.checkpoint == null ? (e.timer.reset(), e.player.removeCheckpoint()) : e.player.respawn(true)
-        // END Doppler's code
+	var originalSetPositionToCheckpoint = app.player.setPositionToCheckpoint;
+    app.player.setPositionToCheckpoint = function() {
+		if (app.player.checkpoint != null) {
+			beforeCheckpointRespawn.forEach((func) => func());
 
-        if (t == false) afterLevelRestart.forEach((func) => func());
-    }
+			// START Doppler's code
+			app.player.checkpoint != null &&
+			app.player.setPosition({
+			  x: app.player.position.x = app.player.checkpoint.x,
+			  y: app.player.position.y = app.player.checkpoint.y,
+			  z: app.player.position.z = app.player.checkpoint.z
+			}, !1)
+			// END Doppler's code
 
-    app.player.restart = function() {
-        beforeCheckpointRespawn.forEach((func) => func());
-
-        // START Doppler's code
-        app.level.retryLevel(app, true);
-        // END Doppler's code
-
-        afterCheckpointRespawn.forEach((func) => func());
+			afterCheckpointRespawn.forEach((func) => func());
+		}
     }
 
     app.player.finish = function() {
