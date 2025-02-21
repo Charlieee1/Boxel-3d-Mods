d74b266c787440791b830e5984c7af59f6b5e9de
Charlieee1 <77288859+Charlieee1@users.noreply.github.com>
2025-02-21 07:30:15 -0800

    add compatibility with the new verified checkmark

--- Boxel 3d Time Manipulator.user.js
+++ Boxel 3d Time Manipulator.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Boxel 3d Time Manipulator
 // @namespace    http://tampermonkey.net/
-// @version      v1.2
+// @version      v1.2.1
 // @description  Time manipulator library for boxel 3d
 // @author       Charlieee1
-// @match        *dopplercreative.com/test/*
+// @match        *dopplercreative.com/boxel-3d/play*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -50,77 +50,53 @@ function resetTime() {
     app.timer.reset = function() {
         beforeLevelStart.forEach((func) => func());
 
-		// START Doppler's code
-		var e = Date.now();
-		app.timer.startTime = e,
-		app.timer.pauseTime = e,
-		app.timer.playTime = 0
-		// END Doppler's code
+        // START Doppler's code
+        var e = Date.now();
+        app.timer.startTime = e,
+        app.timer.pauseTime = e,
+        app.timer.playTime = 0
+        // END Doppler's code
 
         afterLevelStart.forEach((func) => func());
     }
 
-	// HELP I can't figure out how to detect level restart
-	// This implementation detects level starts (above function) and checkpoint respawns (next function) - so it's useless
+    // HELP I can't figure out how to detect level restart
+    // This implementation detects level starts (above function) and checkpoint respawns (next function) - so it's useless
     //app.level.resetLevel = function() {
     //    beforeLevelRestart.forEach((func) => func());
-	//
-	//	// START Doppler's code
-	//	for (var e = 0; e < app.level.children.length; e++) {
-	//	    var t = app.level.children[e];
-	//	    t.resetToOrigin()
-	//	}
-	//	// END Doppler's code
-	//
+    //
+    //    // START Doppler's code
+    //    for (var e = 0; e < app.level.children.length; e++) {
+    //        var t = app.level.children[e];
+    //        t.resetToOrigin()
+    //    }
+    //    // END Doppler's code
+    //
     //    afterLevelRestart.forEach((func) => func());
     //}
 
-	var originalSetPositionToCheckpoint = app.player.setPositionToCheckpoint;
+    var originalSetPositionToCheckpoint = app.player.setPositionToCheckpoint;
     app.player.setPositionToCheckpoint = function() {
-		if (app.player.checkpoint != null) {
-			beforeCheckpointRespawn.forEach((func) => func());
+        if (app.player.checkpoint != null) {
+            beforeCheckpointRespawn.forEach((func) => func());
 
-			// START Doppler's code
-			app.player.checkpoint != null &&
-			app.player.setPosition({
-			  x: app.player.position.x = app.player.checkpoint.x,
-			  y: app.player.position.y = app.player.checkpoint.y,
-			  z: app.player.position.z = app.player.checkpoint.z
-			}, !1)
-			// END Doppler's code
+            // START Doppler's code
+            app.player.checkpoint != null &&
+            app.player.setPosition({
+              x: app.player.position.x = app.player.checkpoint.x,
+              y: app.player.position.y = app.player.checkpoint.y,
+              z: app.player.position.z = app.player.checkpoint.z
+            }, !1)
+            // END Doppler's code
 
-			afterCheckpointRespawn.forEach((func) => func());
-		}
+            afterCheckpointRespawn.forEach((func) => func());
+        }
     }
 
+    let originalFinish = app.player.finish;
     app.player.finish = function() {
         beforeFinish.forEach((func) => func());
-
-        // START Doppler's code
-        var e = app.level.name,
-            t = app.timer.toString(),
-            n = app.storage.saveScore(e, t),
-            s = "Finished!<br>Score: " + t;
-        app.timer.render(t), n == true && (s += "<br><em>New record!</em>"), app.play = false, window.dispatchEvent(new CustomEvent("openPopup", {
-            detail: {
-                text: s,
-                inputs: [{
-                    value: "Retry",
-                    type: "button",
-                    callback: function() {
-                        app.level.retryLevel(app), window.dispatchEvent(new CustomEvent("closePopup"))
-                    }
-                }, {
-                    value: "Continue",
-                    type: "button",
-                    callback: function() {
-                        app.level.exitLevel(app), window.dispatchEvent(new CustomEvent("closePopup"))
-                    }
-                }]
-            }
-        }))
-        // END Doppler's code
-
+        originalFinish();
         afterFinish.forEach((func) => func());
     }
 
